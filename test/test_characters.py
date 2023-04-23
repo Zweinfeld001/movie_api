bde913cfe3609107453e91434af36e4cda92707f
Zweinfeld001 <97060356+Zweinfeld001@users.noreply.github.com>
2023-04-23 14:42:53 -0700

    Add files via upload

---
+++ test/test_characters.py
@@ -0,0 +1,63 @@
+from fastapi.testclient import TestClient
+
+from src.api.server import app
+
+import json
+
+client = TestClient(app)
+
+prefix = "/Users/zach/Desktop/CSC_365/Assignment3/"
+
+def test_get_character():
+    response = client.get("/characters/7421")
+    assert response.status_code == 200
+
+    with open(prefix + "test/characters/7421.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+
+def test_characters():
+    response = client.get("/characters/")
+    assert response.status_code == 200
+
+    with open(prefix + "test/characters/root.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+# New test case (includes multiple conversation partners)
+def test_get_character2():
+    response = client.get("/characters/2")
+    assert response.status_code == 200
+
+    with open(prefix + "test/characters/2.json", encoding="utf-8") as f:
+        assert response.json() == json.load(f)
+
+
+def test_sort_filter():
+    response = client.get(
+        "/characters/?name=amy&limit=50&offset=0&sort=number_of_lines"
+    )
+    assert response.status_code == 200
+
+    with open(
+        prefix + "test/characters/characters-name=amy&limit=50&offset=0&sort=number_of_lines.json",
+        encoding="utf-8",
+    ) as f:
+        assert response.json() == json.load(f)
+
+# New test case ()
+def test_sort_filter2():
+    response = client.get(
+        "/characters/?name=%20&limit=250&offset=42&sort=movie"
+    )
+    assert response.status_code == 200
+
+    with open(
+        prefix + "test/characters/characters-name=space&limit=250&offset=42&sort=movie.json",
+        encoding="utf-8",
+    ) as f:
+        assert response.json() == json.load(f)
+
+
+def test_404():
+    response = client.get("/characters/400")
+    assert response.status_code == 404
